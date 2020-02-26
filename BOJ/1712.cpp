daf143b6ff160f3067a133751a3a17abeafcf078
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-02-27 00:12:59 +0900

    2020. 02. 27 손익분기점

---
+++ BOJ/1712.cpp
@@ -0,0 +1,30 @@
+﻿/*
+	BOJ 1712 - 손익분기점
+	Created by haejun on 2020/02/27
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+#define ll long long int
+
+ll a, b, c;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> a >> b >> c;
+	if (b >= c) {
+		cout << -1 << "\n";
+		return 0;
+	}
+
+	ll ans = a / (c - b) + 1;
+	cout << ans << "\n";
+
+	return 0;
+}
