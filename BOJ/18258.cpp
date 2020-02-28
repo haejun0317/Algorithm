47feb58a1f854e022a197bd20e9041d41d1013b7
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-02-28 23:19:12 +0900

    2020. 02. 28 큐2

---
+++ BOJ/18258.cpp
@@ -0,0 +1,55 @@
+﻿/*
+	BOJ 18258 - 큐2
+	Created by haejun on 2020/02/28
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int n;
+queue <int> q;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	int a;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		if (s == "push") {
+			cin >> a;
+			q.push(a);
+		}
+		else if (s == "front") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.front() << "\n";
+		}
+		else if (s == "back") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.back() << "\n";
+		}
+		else if (s == "size") {
+			cout << q.size() << "\n";
+		}
+		else if (s == "empty") {
+			if (q.empty()) cout << 1 << "\n";
+			else cout << 0 << "\n";
+		}
+		else if (s == "pop") {
+			if (q.empty()) cout << -1 << "\n";
+			else {
+				cout << q.front() << "\n";
+				q.pop();
+			}
+		}
+	}
+
+	return 0;
+}
