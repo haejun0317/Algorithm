fc15b7b124cd81b0828b40b34c401fc565d0ec41
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-01 23:02:00 +0900

    2020. 03. 01 영수증

---
+++ BOJ/5565.cpp
@@ -0,0 +1,34 @@
+﻿/*
+	BOJ 5565 - 영수증
+	Created by haejun on 2020/03/01
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int arr[10];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int res;
+	int a;
+	int sum = 0;
+
+	cin >> res;
+	for (int i = 0; i < 9; i++) {
+		cin >> a;
+		sum += a;
+	}
+
+	cout << res - sum << "\n";
+
+
+	return 0;
+}
