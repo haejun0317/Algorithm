8467ea5ba0b35ec4e2f07a416d4c25fb6a8149ed
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-01 22:55:43 +0900

    2020. 03. 01 나머지

---
+++ BOJ/3052.cpp
@@ -0,0 +1,33 @@
+﻿/*
+	BOJ 3052 - 나머지
+	Created by haejun on 2020/03/01
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int check[1002];
+int ans;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int a;
+	for (int i = 0; i < 10; i++) {
+		cin >> a;
+		if (check[a % 42] == 0) {
+			check[a % 42] = 1;
+			ans++;
+		}
+	}
+
+	cout << ans << "\n";
+
+	return 0;
+}
