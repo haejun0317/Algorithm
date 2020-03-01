783a2c4ad1a175094a1cdc20ca5c40a6aa9f9910
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-01 23:17:57 +0900

    2020. 03. 01 피보나치 수2

---
+++ BOJ/2748.cpp
@@ -0,0 +1,33 @@
+﻿/*
+	BOJ 2748 - 피보나치 수2
+	Created by haejun on 2020/03/01
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+long long int dp[120];
+int n;
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n] << "\n";
+
+	return 0;
+}
