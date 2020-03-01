927e8c453dd02546df5af76a2ef14980733abea9
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-01 23:04:40 +0900

    2020. 03. 01 피보나치 수

---
+++ BOJ/2747.cpp
@@ -0,0 +1,31 @@
+﻿/*
+	BOJ 2747 - 피보나치 수
+	Created by haejun on 2020/03/01
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int arr[50];
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 1;
+	for (int i = 3; i <= n; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	cout << arr[n] << "\n";
+
+	return 0;
+}
