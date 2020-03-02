6e21640065d9293fccdd4beacc9526b7e5161a33
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-02 21:32:52 +0900

    2020. 03. 02 대회 or 인턴

---
+++ BOJ/2875.cpp
@@ -0,0 +1,35 @@
+﻿/*
+	BOJ 2875 - 대회 or 인턴
+	Created by haejun on 2020/03/02
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int a, b, k, ans;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> a >> b >> k;
+	while (k--) {
+		int aa;
+		if (a == 0 || a == 1) aa = a;
+		else aa = a / 2;
+
+		if (aa >= b) a--;
+		else b--;
+		//cout << a << " " << b << " " << k << "\n";
+	}
+	if (a == 0 || a == 1) cout << 0 << "\n";
+	else cout << min(a / 2, b) << "\n";
+
+
+	return 0;
+}
