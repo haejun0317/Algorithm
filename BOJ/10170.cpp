1db66bd439ee14c1a3c4788199bc58223c6a39e0
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 00:55:25 +0900

    2020. 03. 09 NFC West vs North

---
+++ BOJ/10170.cpp
@@ -0,0 +1,33 @@
+﻿/*
+	BOJ 10170 - NFC West vs North
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "NFC West       W   L  T" << endl;
+	cout << "-----------------------" << endl;
+	cout << "Seattle        13  3  0" << endl;
+	cout << "San Francisco  12  4  0" << endl;
+	cout << "Arizona        10  6  0 " << endl;
+	cout << "St. Louis      7   9  0" << endl;
+	cout << endl;
+	cout << "NFC North      W   L  T" << endl;
+	cout << "-----------------------" << endl;
+	cout << "Green Bay      8   7  1" << endl;
+	cout << "Chicago        8   8  0" << endl;
+	cout << "Detroit        7   9  0" << endl;
+	cout << "Minnesota      5  10  1" << endl;
+
+	return 0;
+}
