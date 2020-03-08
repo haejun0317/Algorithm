1c1c47eb5acef1b543ef81a317567d483e15ac36
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 00:40:12 +0900

    2020. 03. 09 선 그리기

---
+++ BOJ/16396.cpp
@@ -0,0 +1,37 @@
+﻿/*
+	BOJ 16396 - 선 그리기
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int arr[10002];
+int n;
+int m;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		if (b > m) m = b;
+		for (int j = a; j < b; j++) arr[j] = 1;
+	}
+
+	int ans = 0;
+	for (int i = 0; i <= m; i++) if (arr[i] == 1) ans++;
+
+	cout << ans << "\n";
+
+
+	return 0;
+}
