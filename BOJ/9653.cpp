4492864efea851efbf86a6275c197376879b4aa0
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 01:03:49 +0900

    2020. 03. 09  스타워즈 로고

---
+++ BOJ/9653.cpp
@@ -0,0 +1,31 @@
+﻿/*
+	BOJ 9653 - 스타워즈 로고
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "    8888888888  888    88888" << endl;
+	cout << "   88     88   88 88   88  88" << endl;
+	cout << "    8888  88  88   88  88888" << endl;
+	cout << "       88 88 888888888 88   88" << endl;
+	cout << "88888888  88 88     88 88    888888" << endl;
+	cout << endl;
+	cout << "88  88  88   888    88888    888888" << endl;
+	cout << "88  88  88  88 88   88  88  88" << endl;
+	cout << "88 8888 88 88   88  88888    8888" << endl;
+	cout << " 888  888 888888888 88  88      88" << endl;
+	cout << "  88  88  88     88 88   88888888" << endl;
+
+	return 0;
+}
