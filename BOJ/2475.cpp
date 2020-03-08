86b7cf0c0b71d32f672bc021bc5a6e06f23c25aa
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 01:10:06 +0900

    2020. 03. 09 검증수

---
+++ BOJ/2475.cpp
@@ -0,0 +1,27 @@
+﻿/*
+	BOJ 2475 - 검증수
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a, sum=0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> a;
+		sum += a * a;
+	}
+	cout << sum % 10 << "\n";
+
+	return 0;
+}
