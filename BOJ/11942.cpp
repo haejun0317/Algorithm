edc5b209ad1c0060160d184078e9885154b684e2
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 01:06:19 +0900

    2020. 03. 09 고려대는 사랑입니다

---
+++ BOJ/11942.cpp
@@ -0,0 +1,21 @@
+﻿/*
+	BOJ 11942 - 고려대학교
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "고려대학교\n";
+
+	return 0;
+}
