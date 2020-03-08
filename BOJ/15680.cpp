0b7b6feb215a63e34c3a0100a3edc80374d0d468
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 01:14:29 +0900

    2020. 03. 09 연세대학교

---
+++ BOJ/15680.cpp
@@ -0,0 +1,24 @@
+﻿/*
+	BOJ 15680 - 연세대학교
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	if (n == 0) cout << "YONSEI\n";
+	else cout << "Leading the Way to the Future\n";
+
+	return 0;
+}
