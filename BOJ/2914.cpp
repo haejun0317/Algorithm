0c1b1bf6b8bec37ab9eda9ca9151ca9441b45519
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 01:02:11 +0900

    2020. 03. 09 저작권

---
+++ BOJ/2914.cpp
@@ -0,0 +1,23 @@
+﻿/*
+	BOJ 2914 - 저작권
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * (b - 1) + 1 << "\n";
+
+	return 0;
+}
