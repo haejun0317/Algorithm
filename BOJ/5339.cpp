2a9077a9579903cc4ce004603a64d1271dbcbd97
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 00:53:23 +0900

    2020. 03. 09 콜 센터

---
+++ BOJ/5339.cpp
@@ -0,0 +1,31 @@
+﻿/*
+	BOJ 5339 - 콜 센터
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "     /~\\" << '\n';
+	cout << "    ( oo|" << '\n';
+	cout << "    _\\=/_" << '\n';
+	cout << "   /  _  \\" << '\n';
+	cout << "  //|/.\\|\\\\ " << '\n';
+	cout << " ||  \\ /  ||" << '\n';
+	cout << "============" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+
+
+	return 0;
+}
