d7a8c1a48af891ae3c5a0a8e7a5b4126ba390bec
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-09 00:25:08 +0900

    2020. 03. 09 홍익대학교

---
+++ BOJ/16394.cpp
@@ -0,0 +1,26 @@
+﻿/*
+	BOJ 16394 - 홍익대학교
+	Created by haejun on 2020/03/09
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<memory.h>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int s = 1946;
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	cout << n-s;
+	
+
+	return 0;
+}
