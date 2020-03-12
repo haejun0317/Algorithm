5e6513402973ee17bdd017ddea076d491dcf31a4
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-12 14:14:42 +0900

    2020. 03. 12 OX퀴즈

---
+++ BOJ/8958.cpp
@@ -0,0 +1,32 @@
+﻿/*
+	BOJ 8958 - OX퀴즈
+	Created by haejun on 2020/03/12
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int n;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	while (n--) {
+		cin >> s;
+		int cnt = 0;
+		int sum = 0;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'O') {
+				cnt++;
+				sum += cnt;
+			}
+			else cnt = 0;
+		}
+		cout << sum << "\n";
+	}
+
+	return 0;
+}
