2d5502c48d9bca1383b73d73c62a29c451611f40
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-12 14:26:50 +0900

    2020. 03. 12 문자열 반복

---
+++ BOJ/2675.cpp
@@ -0,0 +1,30 @@
+﻿/*
+	BOJ 2675 - 문자열 반복
+	Created by haejun on 2020/03/12
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+string s;
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	while (n--) {
+		int a;
+		cin >> a;
+		cin >> s;
+		for (int i = 0; i < s.size(); i++) {
+			for (int j = 0; j < a; j++) {
+				cout << s[i];
+			}
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
