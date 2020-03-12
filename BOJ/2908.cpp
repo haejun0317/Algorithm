5cbbc18046b78c4c523f78a79fcdb3f328d13f53
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-12 14:33:38 +0900

    2020. 03. 12 상수

---
+++ BOJ/2908.cpp
@@ -0,0 +1,27 @@
+﻿/*
+	BOJ 2908 - 상수
+	Created by haejun on 2020/03/12
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+string a, b;
+string aa, bb;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b;
+	for (int i = a.size()-1; i >=0; i--) aa += a[i];
+	for (int i = b.size() - 1; i >= 0; i--) bb += b[i];
+
+	int an = atoi(aa.c_str());
+	int bn = atoi(bb.c_str());
+
+	if (an > bn) cout << an;
+	else cout << bn;
+
+	return 0;
+}
