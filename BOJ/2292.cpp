1876fd9965b33e6615a6fca6e00b3a2b1e7e6183
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-13 00:16:06 +0900

    2020. 03. 13 벌집

---
+++ BOJ/2292.cpp
@@ -0,0 +1,29 @@
+﻿/*
+	BOJ 2292 - 벌집
+	Created by haejun on 2020/03/13
+*/
+#include<iostream>
+using namespace std;
+
+int n, sum=2, lv;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	
+	if (n == 1) cout << 1;
+	else {
+		while (n >= sum) {
+			sum += 6 * lv;
+			lv++;
+		}
+		cout << lv;
+	}
+
+
+
+	return 0;
+}
