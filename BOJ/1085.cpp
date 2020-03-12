0bb09a39e1a2d00995d7d207124b208ae76244b9
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-13 00:06:36 +0900

    2020. 03. 13 직사각형에서 탈출

---
+++ BOJ/1085.cpp
@@ -0,0 +1,23 @@
+﻿/*
+	BOJ 1085 - 직사각형에서 탈출
+	Created by haejun on 2020/03/13
+*/
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+
+int y, x, h, w;
+int arr[4];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> x >> y >> w >> h;
+	int ans = min(w - x, x);
+	ans = min(ans, min(h - y, y));
+	cout << ans;
+
+	return 0;
+}
