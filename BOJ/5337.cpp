12a3a8f2b4148ceb8d9795d5582fef18c1e35bf5
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 01:20:54 +0900

    2020. 03. 14 웰컴

---
+++ BOJ/5337.cpp
@@ -0,0 +1,17 @@
+﻿/*
+	BOJ 5337 - 웰컴
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cout << ".  .   .\n";
+	cout << "|  | _ | _. _ ._ _  _\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+
+	return 0;
+}
