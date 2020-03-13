413129f7555a266d381a7f3762829c6596237d8d
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-13 23:54:26 +0900

    2020. 03. 13 평균 점수

---
+++ BOJ/10039.cpp
@@ -0,0 +1,24 @@
+﻿/*
+	BOJ 10039 - 평균 점수
+	Created by haejun on 2020/03/13
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int a;
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> a;
+		if (a < 40) a = 40;
+		sum += a;
+	}
+
+	cout<< sum / 5;
+	
+
+	return 0;
+}
