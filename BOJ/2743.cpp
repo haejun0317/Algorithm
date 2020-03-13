11bea3307d24e208037f3d34b17e8a3f7c8b6008
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 01:09:51 +0900

    2020. 03. 14 단어 길이 재기

---
+++ BOJ/2743.cpp
@@ -0,0 +1,18 @@
+﻿/*
+	BOJ 2743 - 단어 길이 재기
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	string s;
+	cin >> s;
+	cout << s.size();
+
+	return 0;
+}
