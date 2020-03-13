a6f3646e021904c3320812ac10fecd62b923da95
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 00:23:20 +0900

    2020. 03. 14 피보나치 수5

---
+++ BOJ/10870.cpp
@@ -0,0 +1,23 @@
+﻿/*
+	BOJ 10870 - 피보나치 수 5
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+using namespace std;
+
+int n;
+int arr[22];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 1;
+	for (int i = 3; i <= 20; i++) arr[i] = arr[i - 1] + arr[i - 2];
+	cout << arr[n];
+
+	return 0;
+}
