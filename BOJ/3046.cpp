0fa418744383a43dec3fe213a39265b7cb93f900
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 00:15:03 +0900

    2020. 03. 14 R2

---
+++ BOJ/3046.cpp
@@ -0,0 +1,19 @@
+﻿/*
+	BOJ 3046 - R2
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int r1, r2, s;
+	cin >> r2 >> s;
+	r1 = (s * 2) - r2;
+	cout << r1;
+
+	return 0;
+}
