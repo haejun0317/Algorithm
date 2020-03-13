c6b6ff304ccc06fd8ba47e4f4b603f241c331d58
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 00:52:52 +0900

    2020. 03. 14 마이크로소프트 로고

---
+++ BOJ/5338.cpp
@@ -0,0 +1,19 @@
+﻿/*
+	BOJ 5338 - 마이크로소프트 로고
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cout << "       _.-;;-._" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-;;-._|" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-''-._|" << endl;
+
+	return 0;
+}
