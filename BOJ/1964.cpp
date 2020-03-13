8846e8110e1fcbfb268cd8cab35f2e547b7c5ea3
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 01:18:42 +0900

    2020. 03. 14 오각형, 오각형, 오각형

---
+++ BOJ/1964.cpp
@@ -0,0 +1,22 @@
+﻿/*
+	BOJ 1964 - 오각형, 오각형, 오각형
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	int ans = 5;
+	for (int i = 1; i < n; i++) ans = (ans + (i + 2) * 3 - 2) % 45678;
+
+	cout << ans;
+
+	return 0;
+}
