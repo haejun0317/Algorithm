fb99fbb7c906c4dc350040f540c52f241904bcf6
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 00:07:54 +0900

    2020. 03. 14 평균은 넘겠지

---
+++ BOJ/4344.cpp
@@ -0,0 +1,39 @@
+﻿/*
+	BOJ 4344 - 평균은 넘겠지
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int arr[1002];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int a;
+		cin >> a;
+		double sum = 0;
+		for (int i = 0; i < a; i++) {
+			cin >> arr[i];
+			sum += arr[i];
+		}
+
+		sum = sum / (double)a;
+		int cnt = 0;
+		for (int i = 0; i < a; i++) {
+			if (arr[i] > sum) cnt++;
+		}
+
+		double ans = (double)cnt / a * 100;
+		cout.precision(3);
+		cout << fixed << ans << "%\n";
+		
+	}
+
+	return 0;
+}
