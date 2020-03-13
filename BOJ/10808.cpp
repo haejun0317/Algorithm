59a2b6aee823dda8fe8d768ea544b349ca3b73ed
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-14 01:02:15 +0900

    2020. 03. 14 알파벳 개수

---
+++ BOJ/10808.cpp
@@ -0,0 +1,24 @@
+﻿/*
+	BOJ 10808 - 알파벳 개수
+	Created by haejun on 2020/03/14
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int check[27];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		char a = s[i];
+		check[a - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) cout << check[i] << " ";
+
+	return 0;
+}
