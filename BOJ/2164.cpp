db99b7d864263d3416b76674a1e3b687a05e9a75
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-27 00:59:44 +0900

    2020. 03. 27 카드2

---
+++ BOJ/2164.cpp
@@ -0,0 +1,34 @@
+﻿﻿/*
+	BOJ 2164 - 카드2
+	Created by haejun on 2020/03/27
+*/
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n;
+queue <int> q;
+
+
+int main() {
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	while (true) {
+		if (q.size() == 1) {
+			cout << q.front();
+			break;
+		}
+
+		q.pop();
+		int temp = q.front();
+		q.pop();
+		q.push(temp);
+	}
+
+
+	return 0;
+}
