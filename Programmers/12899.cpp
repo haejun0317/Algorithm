1555a12ff09cc5a4851e746a53da8d0f96a58bd6
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-03-31 22:50:04 +0900

    2020. 03. 31 124 나라의 숫자

---
+++ Programmers/12899.cpp
@@ -0,0 +1,22 @@
+﻿/*
+	Programmers 12899 - 124 나라의 숫자
+	Created by haejun on 2020/03/31
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+char arr[3] = { '4','1','2' };
+
+string solution(int n) {
+	string answer = "";
+	int a;
+	while (n != 0) {
+		a = n % 3;
+		n = n / 3;
+		if (a == 0) n -= 1;
+		answer = arr[a] + answer;
+	}
+	return answer;
+}
