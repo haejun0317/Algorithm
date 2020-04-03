018b2a02e17229c97cae899ba41932169fec322c
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-04-04 01:16:47 +0900

    2020. 04. 04 가장 큰 수

---
+++ Programmers/42746.cpp
@@ -0,0 +1,28 @@
+﻿/*
+	Programmers 42746 - 가장 큰 수
+	Created by haejun on 2020/04/04
+*/
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(string a, string b) {
+	if (a + b > b + a) return true;
+	return false;
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+
+	vector <string> v;
+	for (int i = 0; i < numbers.size(); i++) v.push_back(to_string(numbers[i]));
+	sort(v.begin(), v.end(), compare);
+	
+	// 예외처리
+	if (v[0] == "0") return "0";
+
+	for (int i = 0; i < v.size(); i++) answer += v[i];
+
+	return answer;
+}
