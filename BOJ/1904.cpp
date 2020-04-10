b775503a810ca0c8040b7f1e538c7f00a88ac62c
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-04-10 23:23:33 +0900

    2020. 04. 10 01타일

---
+++ BOJ/1904.cpp
@@ -0,0 +1,26 @@
+﻿﻿/*
+	BOJ 1904 - 01타일
+	Created by haejun on 2020/04/10
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n;
+long long int arr[1000001];
+
+int main() {
+
+
+	cin >> n;
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		arr[i] = (arr[i - 2] + arr[i - 1]) % 15746;
+
+	}
+	cout << arr[n];
+
+	return 0;
+}
