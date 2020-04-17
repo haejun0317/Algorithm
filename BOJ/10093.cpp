a26f99a68eebcf0a81bb09cbaa4a4116a46b8c34
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-04-17 17:13:39 +0900

    2020. 04. 17 숫자

---
+++ BOJ/10093.cpp
@@ -0,0 +1,34 @@
+﻿﻿﻿/*
+	BOJ 10093 - 숫자
+	Created by haejun on 2020/04/17
+*/
+#include <iostream>
+#include <algorithm>
+#define N 100000
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	long long A, B;
+
+	cin >> A >> B;
+
+	if (A > B) {
+		swap(A, B);
+	}
+
+	if (A != B) {
+		cout << B - A - 1 << "\n";
+		for (long long i = A + 1; i < B; i++) {
+			cout << i << " ";
+		}
+	}
+	else {
+		cout << 0;
+	}
+
+}
