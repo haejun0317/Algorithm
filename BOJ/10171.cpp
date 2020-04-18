98bd4ffc05e5b832aa7ccc4f397cd4a81fc4e8b3
haejun0317 <39889791+haejun0317@users.noreply.github.com>
2020-04-18 23:47:02 +0900

    2020. 04. 18 고양이

---
+++ BOJ/10171.cpp
@@ -0,0 +1,18 @@
+﻿﻿/*
+	BOJ 10171 - 고양이
+	Created by haejun on 2020/04/18
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	cout << "\\    \/\\" << endl;
+	cout << " )  ( \')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)\|" << endl;
+
+
+
+	return 0;
+}
